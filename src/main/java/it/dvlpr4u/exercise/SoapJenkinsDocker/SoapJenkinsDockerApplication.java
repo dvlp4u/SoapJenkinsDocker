a24405a53043e8d4b9b70423971986c507542c85
slamalfa <slamalfa@imolainformatica.it>
2021-06-21 16:48:52 +0200

    Commit di prova

---
+++ src/main/java/it/dvlpr4u/exercise/SoapJenkinsDocker/SoapJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package it.dvlpr4u.exercise.SoapJenkinsDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/dvlpr4u/exercise/SoapJenkinsDocker/SoapJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dvlpr4u.exercise.SoapJenkinsDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
